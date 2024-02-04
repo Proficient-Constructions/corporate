9a79e103808be6bd0febfdc3970a459ef31792b9
Raman Bhandari <ramanbhandarieoes@gmail.com>
2024-02-04 02:37:43 +0000

    Create new react app, started working on the header component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../src/components/header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
