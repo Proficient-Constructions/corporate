d1ed85db2e032aada7637b208d437409b8444c37
Jaskaranjot-Khangura <126057614+Jaskaranjot-Khangura@users.noreply.github.com>
2024-02-11 15:29:07 -0600

    footer initialization.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import Header from '../src/components/header/header';
-import Footer from './components/footer';
+import Footer from './components/footer/footer';
 import AppRoutes from './router';
 import './App.css';
 
